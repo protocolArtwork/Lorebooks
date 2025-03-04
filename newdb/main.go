2e6489dd0245e0487a9438162e3c58ba08b9d89a
Terra Verbis <protocolArtwork@gmail.com>
2025-03-04 11:44:50 -0500

    Added newdb command module

---
+++ newdb/main.go
@@ -0,0 +1,18 @@
+package newdb
+
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+func fromCommandLine() {
+	name := flag.String("name", "World", "the name to use in the greeting")
+	flag.Parse()
+
+	fmt.Printf("Hello, %s!\n", *name)
+
+	if flag.NArg() > 0 {
+		log.Println("Additional arguments:", flag.Args())
+	}
+}
